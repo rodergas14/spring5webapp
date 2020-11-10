79ab3ce338f2c9d37ab4dc0f3029e0f6ec3c5af8
rodergas14 <robertalmar@gmail.com>
2020-11-10 19:12:21 +0100

    View books list

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,6 @@ public class BookController {
 	@RequestMapping("/books")
 	public String getBooks(Model model) {
 		model.addAttribute("books", bookRepository.findAll());
-		return "books";
+		return "books/list";
 	}
 }
